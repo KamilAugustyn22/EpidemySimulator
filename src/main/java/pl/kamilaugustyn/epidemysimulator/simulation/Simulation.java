aad8cf739bdee743375d03c125a7f2677ae4063c
KamilAugustyn22 <augustynkamil15@gmail.com>
2022-08-25 13:14:19 +0200

    first commit

--- src/main/java/pl/kamilaugustyn/epidemysimulator/simulation/Simulation.java
+++ src/main/java/pl/kamilaugustyn/epidemysimulator/simulation/Simulation.java
@@ -1,4 +1,26 @@
 package pl.kamilaugustyn.epidemysimulator.simulation;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@Entity(name = "simulations")
 public class Simulation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private int infected;
+    private int r;
+    private double mortality;
+    private int ti;
+    private int tm;
+    private long ts;
+
 }
--- src/main/java/pl/kamilaugustyn/epidemysimulator/simulation/SimulationRepository.java
+++ src/main/java/pl/kamilaugustyn/epidemysimulator/simulation/SimulationRepository.java
@@ -1,6 +1,8 @@
 package pl.kamilaugustyn.epidemysimulator.simulation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SimulationRepository extends JpaRepository<Simulation, Long> {
 }
